445b36d5da464300b101bdcecee172fd382c20a5
sanjid073 <sanjidmahe@gmail.comm>
2022-09-08 18:27:38 +0600

    sfhdjf

--- index.js
+++ index.js
@@ -37,19 +37,62 @@ function dragLeave() {
   this.style.border = "0px black";
 }
 
+
+
 const ans1 = "handbook";
 const ans2 = "based";
 const ans3 = "carefully";
 
-function check(){
-   const result = document.getElementById('ans').innerText;
-   const result1 = document.getElementById('ans1').innerText;
-   const result2 = document.getElementById('ans2').innerText;
-   let final ;
-  
-   if(result === ans1 && result1 === ans2 && result2 === ans3){
-    alert("Right Answer")
-   }else{
-    alert("wrong Answer")
-   }
+const wordArry = [
+  { answer1: ["handbook", "based", "carefully"] },
+  { answer2: ["based","handbook", "carefully"] },
+];
+
+let final;
+
+function check() {
+  const result = document.getElementById("ans").innerText;
+  const result1 = document.getElementById("ans1").innerText;
+  const result2 = document.getElementById("ans2").innerText;
+  if (
+    result === wordArry[0].answer1[0] &&
+    result1 === wordArry[0].answer1[1] &&
+    result2 === wordArry[0].answer1[2]
+  ) {
+    document.getElementById("nxt-btn").style.display = "none";
+    document.getElementById("submit-btn").style.display = "inline-block";
+   document.getElementById("fill-in-gap-box").style.display = "none";
+   document.getElementById("fill-in-gap-box2").style.display = "block";
+   final = 1;
+  } else {
+    document.getElementById("nxt-btn").style.display = "none";
+    document.getElementById("submit-btn").style.display = "inline-block";
+    document.getElementById("fill-in-gap-box").style.display = "none";
+   document.getElementById("fill-in-gap-box2").style.display = "block";
+    final = 0;
+  }
+  return final;
+}
+console.log(wordArry[1].answer2[0] );
+function submit() {
+  const result = document.getElementById("ans3").innerText;
+const result1 = document.getElementById("ans4").innerText;
+const result2 = document.getElementById("ans5").innerText;
+ document.getElementById("info-text").style.display = "none";
+  if (
+    result === wordArry[1].answer2[0] &&
+    result1 === wordArry[1].answer2[1] &&
+    result2 === wordArry[1].answer2[2]
+  ) {
+    let score = final + 1;
+   document.getElementById("fill-in-gap-box2").style.display = "none";
+   document.getElementById("score").style.display = "block";
+   document.getElementById("score").innerText = `your score ${score}`;
+ 
+  } else {
+    let score = final + 0;
+    document.getElementById("fill-in-gap-box2").style.display = "none";
+    document.getElementById("score").style.display = "block";
+    document.getElementById("score").innerText = `your score ${score}`;
+  }
 }
